9e11a914814d80f88a94aeaf3692defa2dd6e51c
Ruslan <r.chudinov@g.nsu.ru>
2025-10-05 05:29:14 +0700

    add navigation logix between activities

--- app/src/main/java/ru/rchudinov/rustore/AppsActivity.kt
+++ app/src/main/java/ru/rchudinov/rustore/AppsActivity.kt
@@ -1,14 +1,22 @@
 package ru.rchudinov.rustore
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class AppsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_games_main)
+        setContentView(R.layout.activity_apps_main)
         enableEdgeToEdge()
+
+        val imageView: ImageView = findViewById(R.id.games_icon)
+        imageView.setOnClickListener {
+            val intent = Intent(this, GamesActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/ru/rchudinov/rustore/GamesActivity.kt
+++ app/src/main/java/ru/rchudinov/rustore/GamesActivity.kt
@@ -1,6 +1,8 @@
 package ru.rchudinov.rustore
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +12,11 @@ class GamesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_games_main)
         enableEdgeToEdge()
+        val myImageView: ImageView = findViewById(R.id.apps_icon)
+        myImageView.setOnClickListener {
+            val intent = Intent(this, AppsActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
