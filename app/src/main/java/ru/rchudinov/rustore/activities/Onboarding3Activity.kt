7de8091077b7e86b703d3d94ae516e8067166584
Ruslan <r.chudinov@g.nsu.ru>
2025-10-05 08:52:56 +0700

    last onboarding screen

--- app/src/main/java/ru/rchudinov/rustore/activities/Onboarding3Activity.kt
+++ app/src/main/java/ru/rchudinov/rustore/activities/Onboarding3Activity.kt
@@ -6,35 +6,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import ru.rchudinov.rustore.R
 import java.io.IOException
 
-class Onboarding2Activity : AppCompatActivity() {
+class Onboarding3Activity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_apps_main)
-        imageView = findViewById(R.id.imageView4)
+        setContentView(R.layout.activity_gos)
+        enableEdgeToEdge()
+
+        imageView = findViewById(R.id.imageView5)
         showMascot("mascot.png")
 
-        var frameLayout: FrameLayout = findViewById(R.id.dimScreen)
+        var frameLayout: FrameLayout = findViewById(R.id.dimScreen2)
         frameLayout.visibility = View.VISIBLE
-
-        val layout : ConstraintLayout = findViewById(R.id.main)
+        val layout : ConstraintLayout = findViewById(R.id.gos_main)
         layout.setOnClickListener {
             frameLayout.visibility = View.GONE
-            frameLayout = findViewById(R.id.dimScreen2)
-            frameLayout.visibility = View.VISIBLE
-            imageView = findViewById(R.id.imageView5)
-            showMascot("mascot.png")
-            layout.setOnClickListener {
-                frameLayout.visibility = View.GONE
-                startActivity(Intent(this, Onboarding3Activity::class.java))
-            }
+            startActivity(Intent(this, GamesActivity::class.java))
         }
-
     }
 
     private fun showMascot(fileName: String) {
