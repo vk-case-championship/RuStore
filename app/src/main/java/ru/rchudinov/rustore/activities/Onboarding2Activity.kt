f302e9459f5fa19504509ed7e410b781f5cd46bf
Ruslan <r.chudinov@g.nsu.ru>
2025-10-05 08:19:25 +0700

    onboarding2

--- app/src/main/java/ru/rchudinov/rustore/activities/Onboarding2Activity.kt
+++ app/src/main/java/ru/rchudinov/rustore/activities/Onboarding2Activity.kt
@@ -1,4 +1,34 @@
 package ru.rchudinov.rustore.activities
 
-class Onboarding2Activity {
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import ru.rchudinov.rustore.R
+import java.io.IOException
+
+class Onboarding2Activity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apps_main)
+        imageView = findViewById(R.id.imageView4)
+        showMascot("mascot.png")
+
+        val frameLayout: FrameLayout = findViewById(R.id.dimScreen)
+        frameLayout.visibility = View.VISIBLE
+    }
+
+    private fun showMascot(fileName: String) {
+        val assetManager = assets
+        try {
+            val inputStream = assetManager.open("images/$fileName")
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            imageView.setImageBitmap(bitmap)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
--- app/src/main/java/ru/rchudinov/rustore/activities/StartOnboardingActivity.kt
+++ app/src/main/java/ru/rchudinov/rustore/activities/StartOnboardingActivity.kt
@@ -1,10 +1,12 @@
 package ru.rchudinov.rustore.activities
 
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import ru.rchudinov.rustore.R
 import java.io.IOException
 
@@ -15,9 +17,12 @@ class StartOnboardingActivity :  AppCompatActivity() {
         setContentView(R.layout.activity_onboarding)
         imageView = findViewById(R.id.imageView4)
         showMascot("mascot.png")
-
+        val layout : ConstraintLayout = findViewById(R.id.onboarding1)
+        layout.setOnClickListener {
+            startActivity(Intent(this, Onboarding2Activity::class.java))
+        }
     }
-    
+
     private fun showMascot(fileName: String) {
         val assetManager = assets
         try {
